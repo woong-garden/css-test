d63cf80f775c861d87836a94ddcac4f22b71d3bc
woong-garden <dndwn7007@gmail.com>
2023-02-15 15:00:59 +0900

    test commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TailwindComponent from './components/TailwindComponent';
+
+function App() {
+  return (
+    <div>
+      <TailwindComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TailwindComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TailwindComponent(props) {
+    return (
+        <div>
+            <h1 className='text-9xl'>안녕</h1>
+            <button className='bg-blue-300 rounded-xl px-3'>버튼</button>
+        </div>
+    );
+}
+
+export default TailwindComponent;
